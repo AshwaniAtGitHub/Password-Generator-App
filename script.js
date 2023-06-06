e65f07792c1e9b6c2f29924365b1030fa61f125a
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2023-06-07 05:04:55 +0530

    handleSlider, setIndicator, getRndInteger, generateRandomNumber, generateLowercase, generateUppercase, generateSymbol functions are created.

---
+++ script.js
@@ -0,0 +1,54 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copyBtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("#upperCase");
+const lowercaseCheck = document.querySelector("#lowerCase");
+const numbersCheck = document.querySelector("#numbers");
+const symbolsCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelectorAll("input[type=checkbox");
+const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+handleSlider();
+// set strength circle colour to grey 
+
+// set password length 
+
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+}
+
+function setIndicator(color){
+    indicator.style.backgroundColor = color;
+    // shadow 
+}
+
+function getRndInteger(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+
+}
+
+function generateRandomNumber(){
+    return getRndInteger(0,9);
+}
+
+function generateLowercase(){
+    return String.fromCharCode(getRndInteger(97,123));
+}
+
+function generateUppercase(){
+    return String.fromCharCode(getRndInteger(65,91));
+}
+
+function generateSymbol(){
+    const randNum = getRndInteger(0,symbols.length);
+    return symbols.charAt(randNum);
+}
