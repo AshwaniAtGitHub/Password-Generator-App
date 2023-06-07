53ce45bd84f025de4b5c432f54e87cd73319f1cc
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2023-06-07 16:53:44 +0530

    all sub functions are created

--- script.js
+++ script.js
@@ -54,68 +54,72 @@ function generateSymbol(){
 }
 
 // function to calculate strength 
-function calcStrength() {
-        let hasUpper = false;
-        let hasLower = false;
-        let hasNum = false;
-        let hasSym = false;
-        if (uppercaseCheck.checked) hasUpper = true;
-        if (lowercaseCheck.checked) hasLower = true;
-        if (numbersCheck.checked) hasNum = true;
-        if (symbolsCheck.checked) hasSym = true;
+function calcStrength()
+{
+  let hasUpper = false;
+  let hasLower = false;
+  let hasNum = false;
+  let hasSym = false;
+  if (uppercaseCheck.checked) hasUpper = true;
+  if (lowercaseCheck.checked) hasLower = true;
+  if (numbersCheck.checked) hasNum = true;
+  if (symbolsCheck.checked) hasSym = true;
     
-        if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
-        setIndicator("#0f0");
-        } else if (
-        (hasLower || hasUpper) &&
-        (hasNum || hasSym) &&
-        passwordLength >= 6
-        ) {
-        setIndicator("#ff0");
-        } else {
-        setIndicator("#f00");
-        }
-     }
+  if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8)
+  {
+    setIndicator("#0f0");
+  }
+  else if ( (hasLower || hasUpper) && (hasNum || hasSym) && passwordLength >= 6 )
+  {
+    setIndicator("#ff0");
+  }
+  else
+  {
+    setIndicator("#f00");
+  }
+}
 
-    // function to copy the content to clipboard 
-    async function copyContent() {
-        try {
-          await navigator.clipboard.writeText(passwordDisplay.value);
-          copyMsg.innerText = "Copied";
-        } catch (err) {
-          copyMsg.innerText = "Failed";
-        }
-        copyMsg.classList.add("active");
-        setTimeout(() => {
-          copyMsg.classList.remove("active");
-        }, 2000);
-      }
-      
-      function handleCheckBoxChange() {
-        checkCount = 0;
-        allCheckBox.forEach((checkbox) => {
-          if(checkbox.checked)
-          checkCount++;
-        });
+// function to copy the content to clipboard 
+async function copyContent()
+{
+  try {
+    await navigator.clipboard.writeText(passwordDisplay.value);
+    copyMsg.innerText = "Copied";
+  } catch (err) {
+    copyMsg.innerText = "Failed";
+  }
+  copyMsg.classList.add("active");
+  setTimeout(() => {
+    copyMsg.classList.remove("active");
+  }, 2000);
+}
 
-        // Special condition 
+function handleCheckBoxChange() {
+  checkCount = 0;
+  allCheckBox.forEach((checkbox) => {
+    if(checkbox.checked)
+    checkCount++;
+  });
 
-        if(passwordLength<checkCount){
-          passwordLength=checkCount;
-          handleSlider();
-        }
-      }
+  // Special condition 
 
-      allCheckBox.forEach((checkbox) => {
-        checkbox.addEventListener('change', handleCheckBoxChange);
-      })
+  if(passwordLength<checkCount){
+    passwordLength=checkCount;
+    handleSlider();
+  }
+}
 
-      copyBtn.addEventListener("click", () => {
-        if (passwordDisplay.value)
-        copyContent();
-      });
+allCheckBox.forEach((checkbox) => {
+  checkbox.addEventListener('change', handleCheckBoxChange);
+})
 
-      inputSlider.addEventListener('input',(e) => {
-        passwordLength = e.target.value;
-        handleSlider();
-      })
+copyBtn.addEventListener("click", () => {
+  if (passwordDisplay.value)
+  copyContent();
+});
+
+inputSlider.addEventListener('input',(e) => {
+  passwordLength = e.target.value;
+  handleSlider();
+})
+
